3210ff8f6c0f4ebb2adc57c38f7ed73a20a241e5
Александр Амелин <apilot1@yandex.ru>
2024-05-25 02:22:11 +0300

    add 2 Make eslint.config mod lock, json

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0');
+  .version('0.1.0')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1> <filepath2>');
 
   program.parse();
